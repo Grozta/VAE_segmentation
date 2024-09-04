43027c55eb443c5eebe50d331bf649841ab50579
huangjinchaoONDA <grozta@163.com>
2024-09-04 11:03:59 +0800

    生成ACDC数据预处理结果

--- data/data_process.py
+++ data/data_process.py
@@ -4,6 +4,8 @@ import nibabel as nib
 from skimage.transform import resize
 import os
 import glob
+import h5py
+import json
 
 
 def data_prpcess_normal(image_path,label_path,to_path):
@@ -75,9 +77,53 @@ def data_prpcess_normal(image_path,label_path,to_path):
         np.save(path.join(path_prefix, 'label.npy'), Out_label.astype(np.int8))
         np.save(path.join(path_prefix, 'merge.npy'), np.stack((Out_img,Out_label),axis=-1).astype(np.int16))
 
+def data_prpcess_ACDC(from_ACDC_path,to_path):
+    """for ACDC datasets"""
+    if not os.path.exists(to_path):
+        os.makedirs(to_path)
+    with open(from_ACDC_path + f"/train.list", "r") as f1:
+            train_list = f1.readlines()
+    train_list = [item.replace("\n", "") for item in train_list]
+    with open(from_ACDC_path + f"/val.list", "r") as f1:
+            val_list = f1.readlines()
+    val_list = [item.replace("\n", "") for item in val_list]
+    with open(from_ACDC_path + f"/test.list", "r") as f1:
+            test_list = f1.readlines()
+    test_list = [item.replace("\n", "") for item in test_list]
+    
+    total_list = train_list+ val_list + test_list
+    
+    names = glob.glob(path.join(from_ACDC_path,"data",'*.h5'))
+    names.sort()
+    names = [path.split(f)[1] for f in names]
+    dataset_info_dict = {"ACDC_train":[],"ACDC_val":[],"ACDC_test":[]}
+
+    for img_name in names:
+        h5f = h5py.File(path.join(from_ACDC_path,"data", img_name), "r")
+        Out_img = h5f["image"][:]
+        Out_label = h5f["label"][:]
+
+        path_prefix = path.join(to_path, img_name.split('.')[0])
+        if not os.path.exists(path_prefix):
+            os.makedirs(path_prefix)
+        np.save(path.join(path_prefix, 'img.npy'),Out_img)
+        np.save(path.join(path_prefix, 'label.npy'), Out_label.astype(np.int8))
+        np.save(path.join(path_prefix, 'merge.npy'), np.stack((Out_img,Out_label),axis=-1))
+        if img_name.split('.')[0] in train_list:
+            dataset_info_dict["ACDC_train"].append(path.join(img_name.split('.')[0], 'merge.npy'))
+        if img_name.split('.')[0] in val_list:
+            dataset_info_dict["ACDC_val"].append(path.join(img_name.split('.')[0], 'merge.npy'))
+        if img_name.split('.')[0] in test_list:
+            dataset_info_dict["ACDC_test"].append(path.join(img_name.split('.')[0], 'merge.npy'))
+    
+    with open(path.join(to_ACDC_path,"Multi_all.json"), 'w') as json_file:
+        json.dump(dataset_info_dict, json_file, indent=4)  # indent=4 格式化输出
+
 if __name__ == "__main__":
     image_path = '<path-to-the-image>/nih_data/Pancreas-CT/data' # TODO: modify this.
     label_path = '<path-to-the-data>/nih_data/Pancreas-CT/TCIA_pancreas_labels-02-05-2017' # TODO: modify this.
     to_path = 'data/nih' # TODO: modify this.
     # data_prpcess_normal(image_path,label_path,to_path) # for MSD NIH SYN datasets
-    
+    from_ACDC_path = "/media/grozta/SOYO/DATASET/ACDC"
+    to_ACDC_path = "/media/grozta/SOYO/DATASET/ACDC4AVE"
+    data_prpcess_ACDC(from_ACDC_path,to_ACDC_path)
